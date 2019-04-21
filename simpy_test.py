14a42f16c7303cb3580d025e7fe274f728fcd1bb
chris williams <chrisjw1@iastate.edu>
2019-04-21 14:53:36 -0500

    Began Auth logic

--- simpy_test.py
+++ simpy_test.py
@@ -8,6 +8,7 @@ from Crypto.Signature import PKCS1_v1_5
 import json
 import pickle
 
+
 class network_module(object):
     def __init__(self,env:simpy.Environment):
         self.env = env
@@ -69,13 +70,30 @@ class authenticator(network_module):
     def __init__(self,env:simpy.Environment):
         super().__init__(env)
         self.pacemaker_buffer = None
+        self.pacemaker_public_key = None
+        self.private_key = RSA.generate(2048)
 
     def set_pacemaker(self,pacemaker:network_module):
         self.pacemaker_buffer = pacemaker.pending_messages
+        pacemaker.auth_public_key = self.private_key.publickey()
 
     def handle_message(self,message:bytearray):
         # TODO possibly simulate entire BLE packet
         data_field = message
+        print(self.parse_message(message))
+
+    def parse_message(self,encrypted_message:bytearray):
+        decrypt_cipher = PKCS1_OAEP.new(self.private_key)
+        message_dict = self.decrypt_message(encrypted_message,decrypt_cipher)
+        if message_dict['type'] == 'auth_request':
+            verify_cipher = PKCS1_v1_5.new(self.pacemaker_public_key)
+        else:
+            return {'type':'error'}
+
+        if self.verify_signature(message_dict['signature'],verify_cipher,message_dict['time']):
+            return message_dict
+        else:
+            return {'type':'error'}
 
 class pacemaker(network_module):
     def __init__(self,env:simpy.Environment):
@@ -87,11 +105,19 @@ class pacemaker(network_module):
         self.programmer_public_key = None
 
     def handle_message(self,message:bytearray):
-        print(self.parse_regular_op_message(message))
+        message_dict = self.parse_regular_op_message(message)
+        if message_dict['type'] == 'error':
+            return
+        elif message_dict['type'] == 'op_request':
+            self.send(self.create_auth_request_message(),self.auth_buffer)
 
     def set_programmer(self,programmer:network_module):
         self.programmer_buffer = programmer.pending_messages
         programmer.pacemaker_public_key = self.private_key.publickey()
+
+    def set_authenticator(self,authenticator:authenticator):
+        self.auth_buffer = authenticator.pending_messages
+        authenticator.pacemaker_public_key = self.private_key.publickey()
 
     def parse_regular_op_message(self,encrypted_message:bytearray):
         decrypt_cipher = PKCS1_OAEP.new(self.private_key)
@@ -109,10 +135,12 @@ class pacemaker(network_module):
             return {'type':'error'}
 
     def create_auth_request_message(self):
-        challenge = random.getrandbits(128)
+        challenge = bytearray([random.randint(0,255)]*8)
         time = self.env.now
         message_dict = {'type':'auth_request','time':time,'challenge':challenge}
-
+        sign_cipher = PKCS1_v1_5.new(self.private_key)
+        encrypt_cipher = PKCS1_OAEP.new(self.auth_public_key)
+        return self.create_encrypted_message(message_dict,sign_cipher,encrypt_cipher)
 
 
 
@@ -148,6 +176,7 @@ class programmer(network_module):
         return self.create_encrypted_message(message_dict,PKCS1_v1_5.new(self.private_key),PKCS1_OAEP.new(self.pacemaker_public_key))
 
     def handle_message(self,message:bytearray):
+        # TODO finish this
         dec_cipher = PKCS1_OAEP.new(self.private_key)
         decrypted_message = dec_cipher.decrypt(message)
         parsed_message = json.loads(str(decrypted_message,encoding='utf-8'))
@@ -175,8 +204,11 @@ env = simpy.Environment()
 # env.process(n2.run())
 pm = pacemaker(env)
 prg = programmer(env)
+auth = authenticator(env)
+auth.set_pacemaker(pm)
 prg.set_pacemaker(pm)
 pm.set_programmer(prg)
+
 env.process(pm.run())
 env.process(prg.run())
 env.run(until=0.5)
